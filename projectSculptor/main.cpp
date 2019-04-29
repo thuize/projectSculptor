22ad94e14940d1bb6123fdae747c489ffc928486
Felipe Lima <felipelimademedeiros@gmail.com>
2019-04-29 16:57:49 -0300

    Revert "ConstrutorDestrutor"
    This reverts commit 50bf9819e939fd3cd3f0531ce515a9de78c59e04.

--- projectSculptor/main.cpp
+++ projectSculptor/main.cpp
@@ -1,11 +1,9 @@
 #include <iostream>
-#include "sculptor.h"
+
 using namespace std;
 
 int main()
 {
-    int camadas=13,linhas=30,colunas=10;
-    Sculptor a(camadas,linhas,colunas);
-
+    cout << "Hello World!" << endl;
     return 0;
 }
